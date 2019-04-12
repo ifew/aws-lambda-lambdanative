5344599dacec6eb3b8e62d091042ef4815bac32c
iFew <chitpong@gmail.com>
2019-04-12 19:16:21 +0700

    build script

--- src/aws-lambda-lambdanative/build.sh
+++ src/aws-lambda-lambdanative/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dotnet publish -r linux-x64 -c Release
-cp bin/Release/netcoreapp2.2/linux-x64/native/* bootstrap
+#dotnet publish -r linux-x64 -c Release
+cp bin/Release/netcoreapp2.2/linux-x64/native/* output/bootstrap
 # zip package.zip bootstrap
 # aws s3 cp package.zip s3://<bucket>/package.zip
