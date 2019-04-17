de09ae0de0794ad5c991858b00fb1e775b9642de
iFew <chitpong@gmail.com>
2019-04-17 16:33:19 +0700

    update folder package

--- src/aws-lambda-lambdanative/build.sh
+++ src/aws-lambda-lambdanative/build.sh
@@ -2,5 +2,6 @@
 
 dotnet publish -r linux-x64 -c Release
 cp bin/Release/netcoreapp2.2/linux-x64/native/* output/bootstrap
-zip output/package.zip output/bootstrap
-aws s3 cp output/package.zip s3://backend-layer
+cd output
+zip package.zip bootstrap
+aws s3 cp package.zip s3://backend-layer
