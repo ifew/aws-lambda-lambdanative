f02c4da5a5589b62c71bd78978dbf6c072dc5f60
iFew <chitpong@gmail.com>
2019-04-18 10:54:05 +0700

    Update how to setup

--- src/aws-lambda-lambdanative/build.sh
+++ src/aws-lambda-lambdanative/build.sh
@@ -4,4 +4,3 @@ dotnet publish -r linux-x64 -c Release
 cp bin/Release/netcoreapp2.2/linux-x64/native/* output/bootstrap
 cd output
 zip package.zip bootstrap
-aws s3 cp package.zip s3://backend-layer
