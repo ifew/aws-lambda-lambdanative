66bee997287e2648ff3f8e42db42a7857c8f9608
iFew <chitpong@gmail.com>
2019-04-19 16:14:00 +0700

    Update file

--- src/aws-lambda-lambdanative/build_linux.sh
+++ src/aws-lambda-lambdanative/build_linux.sh
@@ -10,4 +10,4 @@ rm -f $(pwd)/package.zip
 docker run --rm -v $(pwd):/app lambdanative
 cp bin/Release/netcoreapp2.2/linux-x64/native/aws-lambda-lambdanative bootstrap
 zip package.zip bootstrap
-# aws s3 cp package.zip s3://backend-layer --profile sansiri
+# aws s3 cp package.zip s3://backend-layer [--profile [PROFILE-NAME]]]
